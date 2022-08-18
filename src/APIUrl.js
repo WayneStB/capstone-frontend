3973bd6608ef4c05b8ff38eab6273216b752f268
WayneStB <103074933+WayneStB@users.noreply.github.com>
2022-08-18 19:16:57 -0400

    Update APIUrl.js

--- src/APIUrl.js
+++ src/APIUrl.js
@@ -1,3 +1,8 @@
-const APIUrl = "http://localhost:3001";
+let APIUrl = "http://localhost:3001";
+
+// handles if we are running heroku
+if (window.location.host.indexOf(".herokuapp.com") !== -1) {
+    APIUrl = "https://dashboard.heroku.com/apps/waynestb-capstone-backend";
+}
 
 export default APIUrl;
