4c6d718cbecbc233db2e2711653352f74922e459
WayneStB <103074933+WayneStB@users.noreply.github.com>
2022-08-23 16:11:45 -0400

    attempt buildpack switch

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import reportWebVitals from "./reportWebVitals";
 import "mdb-react-ui-kit/dist/css/mdb.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
 root.render(
     <React.StrictMode>
         <App />
